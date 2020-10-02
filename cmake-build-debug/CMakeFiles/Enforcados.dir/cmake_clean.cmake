05bf87c8f77f999785bca9d543b5e021a99a41d7
josefilho <iamjose.filho@gmail.com>
2020-10-02 06:24:27 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/Enforcados.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Enforcados.dir/main.cpp.o"
+  "Enforcados"
+  "Enforcados.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Enforcados.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <SFML/Graphics.hpp>
+#include <ctime>
+
+int main() {
+
+    return 0;
+}
